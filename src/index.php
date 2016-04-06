2f34563d2e014a0929ccdee7a1821ce5340fd9d5
Mateus Guimarães <mateus.jatene@gmail.com>
2016-04-06 10:46:17 -0300

    minor fix

--- src/index.php
+++ src/index.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Collection;
 
 require_once '../vendor/autoload.php';
 
-$array = [1, 2, 3];
+$array = [1, 2, 3, 4, 5, 6];
 
 $collection = new Collection($array);
+
+// var_dump($collection->chunk(3)); // => [[1, 2, 3], [4, 5, 6]]
